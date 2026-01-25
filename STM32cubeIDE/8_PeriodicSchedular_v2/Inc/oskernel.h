3d9ca065e87f3337f2d88688db4a2e764e9db577
Lavanya0001 <lavanyaimadabattina@gmail.com>
2026-01-25 18:44:08 +0530

    A simplePriodic Schedular using Hardware Timer

---
+++ STM32cubeIDE/8_PeriodicSchedular_v2/Inc/oskernel.h
@@ -0,0 +1,29 @@
+/*
+ * oskernel.h
+ *
+ *  Created on: Jan 21, 2026
+ *      Author: lavanyaimadabattina
+ */
+#pragma once
+
+#ifndef OSKERNEL_H_
+#define OSKERNEL_H_
+
+#include <stdint.h>
+#include "stm32wb55xx.h"
+
+#define PERIOD	100
+#define TIM2_UIF	(1U << 0);
+
+void Timer2_1Hz_Interrupt_Init(void);
+void osKernelInit(void);
+void oskernelStackInit(int i);
+uint8_t osKernelAddThreads(void(*task0)(void),void(*task1)(void),void(*task2)(void));
+void osKernelLaunch(uint32_t quanta);
+
+void osThreadYield(void);
+void task3(void);
+
+
+
+#endif /* OSKERNEL_H_ */
